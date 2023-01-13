a3b7e8d6083a207aa8895b69809264b91dfec9b6
xy3 <a@xsq.pw>
2023-01-13 20:18:33 +0100

    update from server

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,17 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
+    client: 'mysql',
+	  connection: {
+	        host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'alessia_strapi'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'wolftone14'),
+	  },
+//#connection: {
+		  //      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  //  },
     useNullAsDefault: true,
   },
 });
