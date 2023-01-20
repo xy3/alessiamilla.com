cb5595da81a1d6fdec419d541397f35cbe190f7f
xy3 <a@xsq.pw>
2023-01-20 21:05:07 +0100

    brainfart

--- frontend/src/routes/PostImage.svelte
+++ frontend/src/routes/PostImage.svelte
@@ -89,22 +89,22 @@
             font-style: normal;
             font-weight: bold;
             letter-spacing: 0.0em;
-            font-size: 1.9em;
+            font-size: 4.25vw;
         }
         33% {
             font-family: 'Vegawanty', serif;
             font-style: normal;
-            margin-bottom: -15px;
-            letter-spacing: 0.05em;
-            font-size: 2em;
+            margin-bottom: -7px;
+            letter-spacing: 0.3vw;
+            font-size: 4.2vw;
         }
         66% {
             font-family: 'Lato', sans-serif;
             font-weight: lighter;
             font-style: italic;
-            margin-bottom: 0;
-            letter-spacing: normal;
-            font-size: 2em;
+            margin-bottom: -3px;
+            letter-spacing: 0.2vw;
+            font-size: 4.3vw;
         }
     }
 
@@ -116,102 +116,144 @@
         width: 100%;
     }
 
-      /* Extra small devices (phones, 600px and down) */
-@media only screen and (max-width: 600px) {
-    h3 {
-        text-align: center;
-    }
+/*      !* Extra small devices (phones, 600px and down) *!*/
+/*@media only screen and (max-width: 500px) {*/
+/*    h3 {*/
+/*        text-align: center;*/
+/*        font-size: 1em;*/
+/*    }*/
 
-        @keyframes fontChange {
-        0% {
-            font-family: 'Lato', sans-serif;
-            font-style: normal;
-            font-weight: bold;
-            letter-spacing: 0.0em;
-            font-size: 1.9em;
-        }
-        33% {
-            font-family: 'Vegawanty', serif;
-            font-style: normal;
-            margin-bottom: -15px;
-            letter-spacing: 0.05em;
-            font-size: 2em;
-        }
-        66% {
-            font-family: 'Lato', sans-serif;
-            font-weight: lighter;
-            font-style: italic;
-            margin-bottom: 0;
-            letter-spacing: normal;
-            font-size: 2em;
-        }
-    }
+/*        @keyframes fontChange {*/
+/*        0% {*/
+/*            font-family: 'Lato', sans-serif;*/
+/*            font-style: normal;*/
+/*            font-weight: bold;*/
+/*            letter-spacing: 0.0em;*/
+/*            font-size: 1.05em;*/
+/*        }*/
+/*        33% {*/
+/*            font-family: 'Vegawanty', serif;*/
+/*            font-style: normal;*/
+/*            margin-bottom: -4px;*/
+/*            letter-spacing: 0.1em;*/
+/*            font-size: 1em;*/
+/*        }*/
+/*        66% {*/
+/*            font-family: 'Lato', sans-serif;*/
+/*            font-weight: lighter;*/
+/*            font-style: italic;*/
+/*            margin-bottom: 0;*/
+/*            letter-spacing: 0.03em;*/
+/*            font-size: 1.1em;*/
+/*        }*/
+/*    }*/
 
-            .container {
-        height: 40vw;
-    }
+/*            .container {*/
+/*        height: 40vw;*/
+/*    }*/
 
-}
-
-
-
-
-/* Small devices (portrait tablets and large phones, 600px and up) */
-@media only screen and (min-width: 1024px) {
-    @keyframes fontChange {
-        0% {
-            font-family: 'Lato', sans-serif;
-            font-style: normal;
-            font-weight: bold;
-            letter-spacing: 0.0em;
-            font-size: 2.4em;
-        }
-        33% {
-            font-family: 'Vegawanty', serif;
-            font-style: normal;
-            margin-bottom: -15px;
-            letter-spacing: 0.05em;
-            font-size: 2.5em;
-        }
-        66% {
-            font-family: 'Lato', sans-serif;
-            font-weight: lighter;
-            font-style: italic;
-            margin-bottom: 0;
-            letter-spacing: normal;
-            font-size: 2.5em;
-        }
-    }
-}
+/*}*/
 
 
-    /* Small devices (portrait tablets and large phones, 600px and up) */
-@media only screen and (min-width: 1440px) {
-    @keyframes fontChange {
-        0% {
-            font-family: 'Lato', sans-serif;
-            font-style: normal;
-            font-weight: bold;
-            letter-spacing: 0.0em;
-            font-size: 2.9em;
-        }
-        33% {
-            font-family: 'Vegawanty', serif;
-            font-style: normal;
-            margin-bottom: -15px;
-            letter-spacing: 0.05em;
-            font-size: 3em;
-        }
-        66% {
-            font-family: 'Lato', sans-serif;
-            font-weight: lighter;
-            font-style: italic;
-            margin-bottom: 0;
-            letter-spacing: normal;
-            font-size: 3em;
-        }
-    }
-}
+
+/*      !* Extra small devices (phones, 600px and down) *!*/
+/*@media only screen and (min-width: 501px) {*/
+/*    h3 {*/
+/*        text-align: center;*/
+/*        font-size: 2vw;*/
+/*    }*/
+
+/*        @keyframes fontChange {*/
+/*        0% {*/
+/*            font-family: 'Lato', sans-serif;*/
+/*            font-style: normal;*/
+/*            font-weight: bold;*/
+/*            letter-spacing: 0.0em;*/
+/*            font-size: 4.25vw;*/
+/*        }*/
+/*        33% {*/
+/*            font-family: 'Vegawanty', serif;*/
+/*            font-style: normal;*/
+/*            margin-bottom: -4px;*/
+/*            letter-spacing: 0.1em;*/
+/*            font-size: 4.2vw;*/
+/*        }*/
+/*        66% {*/
+/*            font-family: 'Lato', sans-serif;*/
+/*            font-weight: lighter;*/
+/*            font-style: italic;*/
+/*            margin-bottom: 0;*/
+/*            letter-spacing: 0.03em;*/
+/*            font-size: 4.3vw;*/
+/*        }*/
+/*    }*/
+
+/*                        .container {*/
+/*        height: 30vw;*/
+/*    }*/
+
+/*}*/
+
+
+
+
+
+/*!* Small devices (portrait tablets and large phones, 600px and up) *!*/
+/*@media only screen and (min-width: 1024px) {*/
+/*    @keyframes fontChange {*/
+/*        0% {*/
+/*            font-family: 'Lato', sans-serif;*/
+/*            font-style: normal;*/
+/*            font-weight: bold;*/
+/*            letter-spacing: 0.0em;*/
+/*            font-size: 2.4em;*/
+/*        }*/
+/*        33% {*/
+/*            font-family: 'Vegawanty', serif;*/
+/*            font-style: normal;*/
+/*            margin-bottom: -15px;*/
+/*            letter-spacing: 0.05em;*/
+/*            font-size: 2.5em;*/
+/*        }*/
+/*        66% {*/
+/*            font-family: 'Lato', sans-serif;*/
+/*            font-weight: lighter;*/
+/*            font-style: italic;*/
+/*            margin-bottom: 0;*/
+/*            letter-spacing: normal;*/
+/*            font-size: 2.5em;*/
+/*        }*/
+/*    }*/
+/*}*/
+
+
+/*    !* Small devices (portrait tablets and large phones, 600px and up) *!*/
+/*@media only screen and (min-width: 1440px) {*/
+/*    @keyframes fontChange {*/
+/*        0% {*/
+/*            font-family: 'Lato', sans-serif;*/
+/*            font-style: normal;*/
+/*            font-weight: bold;*/
+/*            letter-spacing: 0.0em;*/
+/*            font-size: 2.9em;*/
+/*        }*/
+/*        33% {*/
+/*            font-family: 'Vegawanty', serif;*/
+/*            font-style: normal;*/
+/*            margin-bottom: -15px;*/
+/*            letter-spacing: 0.05em;*/
+/*            font-size: 3em;*/
+/*        }*/
+/*        66% {*/
+/*            font-family: 'Lato', sans-serif;*/
+/*            font-weight: lighter;*/
+/*            font-style: italic;*/
+/*            margin-bottom: 0;*/
+/*            letter-spacing: normal;*/
+/*            font-size: 3em;*/
+/*        }*/
+/*    }*/
+/*}*/
 
 
 
