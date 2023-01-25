87360b8bd609a965cef3ff2a657cb5199c16cc8a
xy3 <a@xsq.pw>
2023-01-26 00:56:22 +0100

    nav

--- frontend/src/routes/Nav.svelte
+++ frontend/src/routes/Nav.svelte
@@ -18,6 +18,12 @@
         z-index: 10;
     }
 
+    @media only screen and (max-width: 768px) {
+        nav a {
+            font-size: 13px;
+        }
+    }
+
     /* Extra small devices (phones, 600px and down) */
     @media only screen and (max-width: 600px) {
         nav {
